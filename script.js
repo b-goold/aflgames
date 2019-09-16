8ce03f7d6fdc2f9a220b4a9253cd971b0982c1d6
b-goold <brodiegoold@gmail.com>
2019-09-16 18:24:15 +1000

    Team data
    Data now feeds through containing exactly what is needed for each team to displayed in d3.
    Modified csv so that rounds can be handled as integers rather than strings

--- script.js
+++ script.js
@@ -4,12 +4,14 @@
 //Load the data
 d3.csv("data/aflgames.csv", function(d) {
         return {
-            year: d.Year,
-            round: d.Round,
+            year: +d.Year,
+            round: +d.Round,
             team_1: d.Team_1,
             team_2: d.Team_2,
-            elo_1: d.Elo_1_Post,
-            elo_2: d.Elo_2_Post
+            elo_1_pre: +d.Elo_1_Pre,
+            elo_2_pre: +d.Elo_2_Pre,
+            elo_1_post: +d.Elo_1_Post,
+            elo_2_post: +d.Elo_2_Post
         };
     })
     //Prepare the data to display
@@ -22,7 +24,7 @@ d3.csv("data/aflgames.csv", function(d) {
         ];
 
         //Setting up the filters we want to return
-        const allowed = ['team_1', 'team_2', 'year', 'round', 'team_1', 'team_2', 'elo_1', 'elo_2'];
+        const allowed = ['team_1', 'team_2', 'year', 'round', 'elo_1', 'elo_2'];
 
         //iterate through each team and return each of their results
         allTeams.forEach(function(team) {
@@ -35,7 +37,51 @@ d3.csv("data/aflgames.csv", function(d) {
         });
     });
 
-function drawLine(team, teamData) {
+function drawLine(team, filteredData) {
+    var lineData;
+    var mergedData = [];
+
     console.log("Generating line for " + team + "...");
 
+    filteredData.forEach(function(teamData) {
+		//if it is a new season, record pre elo
+        if (teamData.round == 1) {
+			if(teamData.team_1 == team) {
+            mergedData.push({
+                'year': teamData.year,
+                'round': 0,
+                'team': teamData.team_1,
+				'elo': teamData.elo_1_pre
+            });
+        }
+		else
+            mergedData.push({
+                'year': teamData.year,
+                'round': 0,
+                'team': teamData.team_2,
+				'elo': teamData.elo_2_pre
+            });
+		}
+		
+		//get post elo for all other rounds
+        if (teamData.team_1 == team) {
+            mergedData.push({
+                'year': teamData.year,
+                'round': teamData.round,
+                'team': teamData.team_1,
+                'elo': teamData.elo_1_post
+            });
+        } else if (teamData.team_2 == team) {
+
+            mergedData.push({
+                'year': teamData.year,
+                'round': teamData.round,
+                'team': teamData.team_2,
+                'elo': teamData.elo_2_post
+            });
+        }
+    });
+
+    console.log("This is the data for " + team + ":");
+    console.log(mergedData);
 };
