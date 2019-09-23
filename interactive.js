acaa46f04e30511cd89581f979e991a6dbb49191
b-goold <brodiegoold@gmail.com>
2019-09-24 00:43:47 +1000

    Dropdown functinality
    dropdown has been added and highlights selected team on the chart with their colour

--- interactive.js
+++ interactive.js
@@ -1,11 +1,34 @@
 const $chart = d3.select('.graphic');
 const $chartInner = d3.select('.graphic__inner');
 const $svg = $chart.select('.graphic__one');
+const $selector = document.getElementById('selector');
+
 
 // const MARGIN = { top: 50, right: 200, bottom: 50, left: 200 };
-const MARGIN = { top: 10, bottom: 30, left: 10, right: 10 };
+const MARGIN = {
+    top: 10,
+    bottom: 30,
+    left: 10,
+    right: 10
+};
 
 // const FONT_SIZE = 12;
+
+function select(selector) {
+    d3.selectAll(".line")
+        .classed("active", false)
+        .classed("inactive", true); //reset any active lines
+
+    var selectedTeam = selector.options[selector.selectedIndex].innerHTML;
+    var teamColour = selector.options[selector.selectedIndex].dataset.colour;
+
+    //find the line for the selected team and applying active selection styling
+    var activeLine = document.getElementById(selectedTeam);
+    activeLine.classList.remove("inactive");
+    activeLine.classList.add("active");
+    d3.select(activeLine)
+        .attr("stroke", teamColour);
+}
 
 function resize() {
 
@@ -31,7 +54,7 @@ function drawChart(width, height) {
     // x axis
     const x = d3.scaleTime()
         .domain([new Date(1897, 0, 1), new Date(2020, 0, 1)])
-        .range([50, width-50]);
+        .range([50, width - 50]);
 
     $svg
         .append('g')
@@ -69,106 +92,109 @@ function drawChart(width, height) {
             .attr("dy", -4))
 
     //--------------- ADD CODE BELOW ------------------ //
-	//Load the data
-d3.csv("data/aflgames.csv", function(d) {
-        return {
-            year: +d.Year,
-			date: d.Date,
-            round: d.Round,
-            team_1: d.Team_1,
-            team_2: d.Team_2,
-            elo_1_pre: +d.Elo_1_Pre,
-            elo_2_pre: +d.Elo_2_Pre,
-            elo_1_post: +d.Elo_1_Post,
-            elo_2_post: +d.Elo_2_Post
-        };
-    })
-    //Prepare the data to display
-    .then(function(data) {
-        //Using the processed csv data
+    //Load the data
+    d3.csv("data/aflgames.csv", function(d) {
+            return {
+                year: +d.Year,
+                date: d.Date,
+                round: d.Round,
+                team_1: d.Team_1,
+                team_2: d.Team_2,
+                elo_1_pre: +d.Elo_1_Pre,
+                elo_2_pre: +d.Elo_2_Pre,
+                elo_1_post: +d.Elo_1_Post,
+                elo_2_post: +d.Elo_2_Post
+            };
+        })
+        //Prepare the data to display
+        .then(function(data) {
+            //Using the processed csv data
 
-        const allTeams = ["Adelaide", "Brisbane Bears", "Brisbane Lions", "Carlton", "Collingwood", "Essendon", "Fitzroy", "Footscray", "Fremantle", "Geelong", "Gold Coast",
-            "Greater Western Sydney", "Hawthorn", "Melbourne", "North Melbourne", "Port Adelaide", "Richmond", "South Melbourne", "St Kilda", "Sydney", "University", "West Coast", "Western Bulldogs"
-        ];
+            const allTeams = ["Adelaide", "Brisbane Bears", "Brisbane Lions", "Carlton", "Collingwood", "Essendon", "Fitzroy", "Footscray", "Fremantle", "Geelong", "Gold Coast",
+                "Greater Western Sydney", "Hawthorn", "Melbourne", "North Melbourne", "Port Adelaide", "Richmond", "South Melbourne", "St Kilda", "Sydney", "University", "West Coast", "Western Bulldogs"
+            ];
 
-        //Setting up the filters we want to return
-        const allowed = ['team_1', 'team_2', 'year', 'date', 'round', 'elo_1', 'elo_2'];
+            //Setting up the filters we want to return
+            const allowed = ['team_1', 'team_2', 'year', 'date', 'round', 'elo_1', 'elo_2'];
 
-        //iterate through each team and return each of their results
-        allTeams.forEach(function(team) {
-            const filtered = data.filter(info => info.team_1 == team || info.team_2 == team);
+            //iterate through each team and return each of their results
+            allTeams.forEach(function(team) {
+                const filtered = data.filter(info => info.team_1 == team || info.team_2 == team);
 
-            //console.log(filtered);
+                //console.log(filtered);
 
-            //Send filtered data to the chart for initial drawing
-            drawLine(team, filtered, x, y);
+                //Send filtered data to the chart for initial drawing
+                drawLine(team, filtered, x, y);
+            });
+
+            select($selector); //load the default selected team
         });
-    });
 
-function drawLine(team, filteredData, xScale, yScale) {
-    var lineData;
-    var mergedData = [];
+    function drawLine(team, filteredData, xScale, yScale) {
+        var lineData;
+        var mergedData = [];
 
-    console.log("Generating line for " + team + "...");
-	var i = 0;
+        console.log("Generating line for " + team + "...");
+        var i = 0;
 
-    filteredData.forEach(function(teamData) {
-		var date = new Date(teamData.date);
-        //if it is a new season, record pre elo
-        if (teamData.round == 1) {
+        filteredData.forEach(function(teamData) {
+            var date = new Date(teamData.date);
+            //if it is a new season, record pre elo
+            if (teamData.round == 1) {
+                if (teamData.team_1 == team) {
+                    mergedData.push({
+                        'date': new Date(date.getYear() + 1900, 00, 01),
+                        'round': 0,
+                        'team': teamData.team_1,
+                        'elo': teamData.elo_1_pre
+                    });
+                } else
+                    mergedData.push({
+                        'date': new Date(date.getYear() + 1900, 00, 01),
+                        'round': 0,
+                        'team': teamData.team_2,
+                        'elo': teamData.elo_2_pre
+                    });
+            }
+
+            //get post elo for all other rounds
             if (teamData.team_1 == team) {
                 mergedData.push({
-                    'date': new Date(date.getYear()+1900,00,01),
-                    'round': 0,
+                    'date': new Date(teamData.date),
+                    'round': teamData.round,
                     'team': teamData.team_1,
-                    'elo': teamData.elo_1_pre
+                    'elo': teamData.elo_1_post
                 });
-            } else
+            } else if (teamData.team_2 == team) {
+
                 mergedData.push({
-                    'date': new Date(date.getYear()+1900,00,01),
-                    'round': 0,
+                    'date': new Date(teamData.date),
+                    'round': teamData.round,
                     'team': teamData.team_2,
-                    'elo': teamData.elo_2_pre
+                    'elo': teamData.elo_2_post
                 });
-        }
+            }
+        });
 
-        //get post elo for all other rounds
-        if (teamData.team_1 == team) {
-            mergedData.push({
-                'date': new Date(teamData.date),
-                'round': teamData.round,
-                'team': teamData.team_1,
-                'elo': teamData.elo_1_post
-            });
-        } else if (teamData.team_2 == team) {
+        //console.log("This is the data for " + team + ":");
+        //console.log(mergedData);
 
-            mergedData.push({
-                'date': new Date(teamData.date),
-                'round': teamData.round,
-                'team': teamData.team_2,
-                'elo': teamData.elo_2_post
-            });
-        }
-    });
+        //prepare the team line
+        var line = d3.line() //translate the line!
+            .x(function(d) {
+                return xScale(d.date);
+            }) // set the x values for the line generator
+            .y(function(d) {
+                return yScale(d.elo);
+            }) // set the y values for the line generator
 
-    //console.log("This is the data for " + team + ":");
-    //console.log(mergedData);
-
-    //prepare the team line
-    var line = d3.line() //translate the line!
-        .x(function(d) {
-            return xScale(d.date);
-        }) // set the x values for the line generator
-        .y(function(d) {
-            return yScale(d.elo);
-        }) // set the y values for the line generator
-
-    //append the path, bind the data, call the line generator
-    $svg.append("path")
-        .datum(mergedData) // 10. Binds data to the line 
-        .attr("class", "line") // Assign a class for styling 
-        .attr("d", line); // 11. Calls the line generator
-};
+        //append the path, bind the data, call the line generator
+        $svg.append("path")
+            .datum(mergedData) // 10. Binds data to the line 
+            .attr("class", "line inactive") // Assign a class for styling 
+            .attr('id', team) //Assign team name for dynamic selection and styling
+            .attr("d", line); // 11. Calls the line generator
+    };
 
 
 
@@ -177,14 +203,13 @@ function drawLine(team, filteredData, xScale, yScale) {
 
 function init() {
 
-            $svg
-                .selectAll('graphic')
-                .append('graphic');
+    $svg
+        .selectAll('graphic')
+        .append('graphic');
 
-            window.addEventListener('resize', resize);
-            resize();
+    window.addEventListener('resize', resize);
+    resize();
 
 }
-
 
 init();
